22df34a04d3ee7000a5cd107ff5395c34fb9be7b
Usuario <correo@ulpgc.es>
2025-06-26 22:35:36 +0100

    Business Unit Correction

--- src/src/main/java/org/example/infrastructure/ports/DataStore.java
+++
@@ -1,7 +0,0 @@
-package org.example.infrastructure.ports;
-import java.sql.SQLException;
-import java.util.List;
-
-public interface DataStore<T> {
-    void insertFlightsIntoDatabase(List<T> items) throws SQLException;
-}
